7882e9c69fde98bbd37666a63f7b59392d44d76f
martinpvz <martin.paniagua200013@gmail.com>
2022-05-13 21:19:14 -0500

    se agrega el codigo y estilos de perfiles, editar perfiles y crear perfiles, ademas de inicio de mainPage

--- main.js
+++ main.js
@@ -20,3 +20,38 @@ var input = '<strong>' + $(this).parents('.dropdown').find('input').val() + '</s
 msg = '<span class="msg">Hidden input value: ';
 $('.msg').html(msg + input + '</span>');
 }); 
+
+
+let editarPerfil = () => {
+  let botones = document.getElementsByClassName("edit-image");
+  for (let boton of botones) {
+    boton.style.display = "block";
+  }
+  let perfiles = document.getElementsByClassName("profile");
+  for (let perfil of perfiles) {
+    perfil.href = "./editProfile.html"
+    perfil.style.animation = "tilt-shaking 0.5s infinite";
+  }
+  let agregar = document.getElementById("add-profile");
+  agregar.style.display = "none";
+  document.getElementById("create-text").innerHTML = "¿Qué perfil deseas modificar?";
+  document.getElementById("editarPerfil").innerHTML = "Cancelar";
+  document.getElementById("editarPerfil").style.backgroundColor = "#ee2222";
+  document.getElementById("editarPerfil").onclick = () => {
+    let botones = document.getElementsByClassName("edit-image");
+    for (let boton of botones) {
+      boton.style.display = "none";
+    }
+    let perfiles = document.getElementsByClassName("profile");
+    for (let perfil of perfiles) {
+      perfil.href = "./mainPage.html"
+      perfil.style.animation = "none";
+    }
+    let agregar = document.getElementById("add-profile");
+    agregar.style.display = "block";
+    document.getElementById("editarPerfil").innerHTML = "Editar perfiles";
+    document.getElementById("create-text").innerHTML = "¿Quién eres?";
+    document.getElementById("editarPerfil").style.backgroundColor = "#ee5622";
+    document.getElementById("editarPerfil").onclick = editarPerfil;
+  }
+}
